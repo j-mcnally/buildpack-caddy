9e7a93f8f9b6d63a7814325870a5440a65fa2b61
Carlos Rivero Aro <carlosriveroaro7@gmail.com>
2017-05-12 01:55:43 +0200

    Update setup_caddy.sh

--- setup_caddy.sh
+++ setup_caddy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
+echo 0.0.0.0:$PORT > HerokuCaddyfile
+echo "import ./Caddyfile" >> HerokuCaddyfile
+
 wget -O caddy.tar.gz https://caddyserver.com/download/linux/amd64?plugins=$PLUGINS
 tar -xvzf caddy.tar.gz
 rm -r init && rm -f *.txt && rm -f *.tar.gz
-
-echo 0.0.0.0:$PORT > HerokuCaddyfile
-echo "import ./Caddyfile" >> HerokuCaddyfile
